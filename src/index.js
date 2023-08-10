3ccd8ffb724d315c76bfc2dc025f78bd027a8091
Mirac-ops <myetkin@web.de>
2023-08-10 15:07:35 +0200

    be-record

---
+++ src/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const low = require("lowdb");
+const FileSync = require("lowdb/adapters/FileSync");
+
+const app = express();
+const port = 3000;  
+
+const adapter = new FileSync("db.json");
+const db = low(adapter);
+
+db.defaults({ records: [] }).write(); 
+app.use(express.json());
+
+
+app.get("/api/records", (req, res) => {
+  const records = db.get("records").value();
+  res.json(records);
+});
+
+app.post("/api/records", (req, res) => {
+  const newRecord = req.body;
+  db.get("records").push(newRecord).write();
+  res.json(newRecord);
+    
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+
+
+const read = () => {
+    const allRecords = db.get('records').value();
+    console.log(allRecords)
+};
+
+read();
