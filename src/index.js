f273ef8753ff8b462716c34139d5f0f75a178dce
Mirac-ops <myetkin@web.de>
2023-08-22 19:15:51 +0200

    be-record-for next-day

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const low = require("lowdb");
 const FileSync = require("lowdb/adapters/FileSync");
 
+
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = 3000;  
+const dotenv = require('dotenv').config();
+const PORT = process.env.PORT;
   
 const myMiddleware = require('./middleware/corsMiddleware');
 
@@ -15,30 +17,37 @@ const db = low(adapter);
 db.defaults({ records: [] }).write(); 
 app.use(express.json());
 
+
 app.use(myMiddleware);
 
 app.get("/api/records", (req, res) => {
+  const newRecord = req.body;
   const records = db.get("records").value();
   res.json(records);
 });
 
 app.post("/api/records", (req, res) => {
   const newRecord = req.body;
+  console.log(newRecord);
   db.get("records").push(newRecord).write();
   res.json(newRecord);
+  //res.status(201).send('Record added');
     
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+const users = require('./routes/users.js');
+const order = require('./routes/orders.js');
+const record = require('./routes/records.js');
+
+app.use('/users', users);
+app.use('/orders', order);
+app.use('/records', record);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
   });
 
 
-const read = () => {
-    const allRecords = db.get('records').value();
-    console.log(allRecords)
-};
 
-read();
 
 // git commit -m " bebebe-jsjjs "
--- src/middleware/corsMiddleware.js
+++ src/middleware/corsMiddleware.js
@@ -1,4 +1,4 @@
-const myMiddleware = (req, res, next) => {
+/* const myMiddleware = (req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header(
         "Access-Control-Allow-Headers",
@@ -7,5 +7,18 @@ const myMiddleware = (req, res, next) => {
     next();
   };
 
-  module.exports = myMiddleware;
+  module.exports = myMiddleware; */
+
+  const myMiddleware = (req, res, next) => 
+{   
+
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+
+    console.log("TIME", Date.now());
+    console.log("Middleware läuft");
+    next();
+};
   
+module.exports = myMiddleware;
